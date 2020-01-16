90fe50eb71832de738aef3f43004c015b4a1a5a1
zhangyb <18768172680@163.com>
2020-01-16 15:54:46 +0800

    [TEST] `NameServerInstanceTest`中新增 `main` 方法用于启动 Name Server.

--- namesrv/src/test/java/org/apache/rocketmq/namesrv/NameServerInstanceTest.java
+++ namesrv/src/test/java/org/apache/rocketmq/namesrv/NameServerInstanceTest.java
@@ -16,6 +16,7 @@
  */
 package org.apache.rocketmq.namesrv;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.rocketmq.common.namesrv.NamesrvConfig;
 import org.apache.rocketmq.remoting.netty.NettyServerConfig;
 import org.junit.After;
@@ -43,5 +44,18 @@ public class NameServerInstanceTest {
             nameSrvController.shutdown();
         }
         //maybe need to clean the file store. But we do not suggest deleting anything.
+    }
+
+    // ====================================
+    // Main.
+    public static void main(String[] args) throws Exception {
+        final NettyServerConfig nettyServerConfig = new NettyServerConfig();
+        nettyServerConfig.setListenPort(9876);
+        final NamesrvConfig namesrvConfig = new NamesrvConfig();
+
+        final NamesrvController nameServer = new NamesrvController(namesrvConfig, nettyServerConfig);
+        nameServer.initialize();
+        nameServer.start();
+        Thread.sleep(DateUtils.MILLIS_PER_DAY);
     }
 }
